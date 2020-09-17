92bf770de5d017e6652a781699302a7a2208307e
utsavprabhakar <utsav.prabhakar@gmail.com>
2020-09-17 13:34:34 +0530

    removed unwanted merge conflict symbols

--- file02.js
+++ file02.js
@@ -4,10 +4,7 @@ console.log('this is yolo')
 console.log('hehe')
 console.log('master commit-1')
 console.log('master commit-2')
-<<<<<<< HEAD
 console.log('feature-3 commit-1')
 console.log('feature-3 commit-2')
-=======
 console.log('master commit 1 after feature-3 creation')
 console.log('master commit 2 after feature-3 creation')
->>>>>>> master
